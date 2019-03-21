81e3e72c3882032d0ba58e7ed8f92f6c71bbd348
anju-abraham <46350051+anju-abraham@users.noreply.github.com>
2019-03-21 15:47:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/lightsensor/HardwareObject.java
@@ -0,0 +1,17 @@
+package com.example.user.lightsensor;
+
+public class HardwareObject
+{
+    private String hardware;
+    private String hardwareMeaning;
+    public HardwareObject(String hardware, String hardwareMeaning) {
+        this.hardware = hardware;
+        this.hardwareMeaning = hardwareMeaning;
+    }
+    public String getHardware() {
+        return hardware;
+    }
+    public String getHardwareMeaning() {
+        return hardwareMeaning;
+    }
+}
